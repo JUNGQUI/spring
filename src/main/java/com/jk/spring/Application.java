756d6c190c961a0404ef83d2e91e48003ace884d
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2020-05-05 16:14:00 +0900

    init

--- src/main/java/com/jk/spring/Application.java
+++ src/main/java/com/jk/spring/Application.java
@@ -1,33 +1,13 @@
 package com.jk.spring;
 
-import com.jk.spring.service.UseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
-    private final UseService useService;
-
-    @Autowired
-    public Application(UseService useService) {
-        this.useService = useService;
-    }
-
-    private static UseService localUseService;
-
-    @PostConstruct
-    private void configure() {
-        localUseService = this.useService;
-    }
-
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		localUseService.useInterface();
-		localUseService.useStartegy();
 	}
 
 }
