c68e1e96bbc1983518dc4f5b30011032c63972be
jklee <ljk2518@gmail.com>
2020-08-10 14:26:19 +0900

    - Test case 분할, set no return test

--- src/main/java/com/jk/spring/pojo/StringPrinter.java
+++ src/main/java/com/jk/spring/pojo/StringPrinter.java
@@ -2,7 +2,7 @@ package com.jk.spring.pojo;
 
 public class StringPrinter implements Printer {
 
-    private StringBuilder stringBuilder = new StringBuilder();
+    private final StringBuilder stringBuilder = new StringBuilder();
 
     public void print(String message) {
         this.stringBuilder.append(message);
--- src/test/java/com/jk/spring/ApplicationTests.java
+++ src/test/java/com/jk/spring/ApplicationTests.java
@@ -20,11 +20,18 @@ public class ApplicationTests {
 
     private final static FastDateFormat FDF = FastDateFormat.getInstance("yyyyMMdd");
 
+    private String globalTest = "";
+
     @Autowired
     private TestObjectRepository testObjectRepository;
 
     @Autowired
     private UseService useService;
+
+    @Test
+    public void POJOTest() {
+
+    }
 
     @Test
     public void contextLoads() {
@@ -33,19 +40,29 @@ public class ApplicationTests {
         useService.useOverLoadingAndRiding("J Tag?");
 //        useService.userIOC_DI();
 
-        // spec 으로 이와 같은 query 를
-        List<TestObject> testObject_byName = testObjectRepository.findByName("JK-Test");
-        // 이렇게 바꿀 수 있다.
-        List<TestObject> testObjects_specByName = testObjectRepository.findAll(TestObjectSpecs.withName("JK-Test"));
+        String test = "";
 
-        // like 검색 시 %%붙여야 하는거 실화냐
-        // jpa repository 기본 설정이 저런가 봄
-        List<TestObject> testObjects_byContent = testObjectRepository.findByContentLike("%데이터가%");
+        setSomething(test); // 당연히 안됨
+        setSomething(globalTest); // 역시 안됨
 
-        Map<TestObjectSpecs.SearchKey, String> condition = new HashMap<>();
-        condition.put(TestObjectSpecs.SearchKey.CONTENT, "데이터가");
-        List<TestObject> testObjects_specByContent = testObjectRepository.findAll(TestObjectSpecs.withSearchKey(condition));
+        System.out.println(test); // ""
+        System.out.println(globalTest); // ""
 
+        setSomething(TestObject.builder().build()); // TestObject 가 정상적으로 변경되었으나 휘발성으로 만들었기에 접근 불가능, code 종료 후 날아감
+        TestObject newTestObject = TestObject.builder().build(); // 새 obj 로 선언
+        setSomething(newTestObject); // 마찬가지로 정상적으로 변경, newTestObject 로 접근 가능
+    }
+
+    // side effect test
+    private void setSomething(String something) {
+        something = "nothing";
+    }
+    private void setSomething(TestObject testObject) {
+        testObject.changeContent("side effect", "set 으로 object를 바꾼 content");
+    }
+
+    @Test
+    public void builderPattern() {
         TestObject saved = TestObject.builder()
                 .name("JK_Test")
                 .content("이야 이거 하나 만드는데 ㅈ빠지게 오래 걸렸다.")
@@ -67,21 +84,23 @@ public class ApplicationTests {
         // DDD 와 builder pattern 의 올바른 수정 방식
         testObjects.get(0).changeContent("JK-Test", "자 데이터가 변경되었다.");
         testObjectRepository.saveAndFlush(testObjects.get(0));
-
-        Calendar calendar = Calendar.getInstance();
-
-        String test = "";
-
-        setSomething(test);
-
-        System.out.println("J Tag");
-
     }
 
-    private void setSomething(String something) {
-        something = "nothing";
-    }
+    @Test
+    public void useSpec() {
+        // spec 으로 이와 같은 query 를
+        List<TestObject> testObject_byName = testObjectRepository.findByName("JK-Test");
+        // 이렇게 바꿀 수 있다.
+        List<TestObject> testObjects_specByName = testObjectRepository.findAll(TestObjectSpecs.withName("JK-Test"));
 
+        // like 검색 시 %%붙여야 하는거 실화냐
+        // jpa repository 기본 설정이 저런가 봄
+        List<TestObject> testObjects_byContent = testObjectRepository.findByContentLike("%데이터가%");
+
+        Map<TestObjectSpecs.SearchKey, String> condition = new HashMap<>();
+        condition.put(TestObjectSpecs.SearchKey.CONTENT, "데이터가");
+        List<TestObject> testObjects_specByContent = testObjectRepository.findAll(TestObjectSpecs.withSearchKey(condition));
+    }
 
     @Test
     public void simpleJavaTest() {
