ee37028391baa48819c59344bf607bff0c0e522b
JUNGQUI <ljk2518@gmail.com>
2020-09-02 16:40:56 +0900

    - final 변수와 static 변수 에 대해 추가 - static method 에 대해 추가

--- src/test/java/com/jk/spring/JKTestNotePad.java
+++ src/test/java/com/jk/spring/JKTestNotePad.java
@@ -1,6 +1,7 @@
 package com.jk.spring;
 
 import com.jk.spring.finalAndStatic.FinalAndStatic;
+import com.jk.spring.finalAndStatic.JKStaticMethod;
 import org.junit.Test;
 
 public class JKTestNotePad {
@@ -20,5 +21,11 @@ public class JKTestNotePad {
 //
 //        System.out.println("second : " + two.nonStaticInteger); // 1
 //        two.getStaticInteger(); // static 이 아닐 경우 1이 예상되나 실제로 static 으로 되어 이미 one 이 생성 될 때 ++ 되었기 때문에 사실상 2가 반환
+
+        // 둘 다 사용 가능하다. 다만 static method 의 경우 별도의 instance 가 필요 없이 (new instance 를 의미) class 내에서 바로 호출 가능하다.
+        // 그 이유는 이미 static 으로 memory 위에 올라가 있고 compile 이 끝난 상태이기 때문이다.
+        JKStaticMethod jkStaticMethod = new JKStaticMethod();
+        jkStaticMethod.staticMethod();
+        JKStaticMethod.staticMethod();
     }
 }
