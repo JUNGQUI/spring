e060932ee5c64b6ce64c5ccd3932eddc732c3e00
JUNGQUI <ljk2518@gmail.com>
2021-05-31 16:11:38 +0900

    stream API chapter 7
    - Fork/Join : recursive task
    
    TODO
    
    - Test code 작성
    - 이후 챕터 진행

---
+++ src/main/java/com/jk/spring/java/modern/chapter7/ForkJoinSumCalculator.java
@@ -0,0 +1,46 @@
+package com.jk.spring.java.modern.chapter7;
+
+import java.util.concurrent.RecursiveTask;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ForkJoinSumCalculator extends RecursiveTask<Long> {
+
+  private final long[] numbers;
+  private final int start;
+  private final int end;
+
+  public static final long THRESHOLD = 10_000;
+
+  public ForkJoinSumCalculator(long[] numbers) {
+    this(numbers, 0, numbers.length);
+  }
+
+  @Override
+  protected Long compute() {
+    int length = end - start;
+
+    if (length <= THRESHOLD) {
+      return computeSequentially();
+    }
+    ForkJoinSumCalculator leftTask = new ForkJoinSumCalculator(numbers, start, start + length/2);
+    ForkJoinSumCalculator rightTask = new ForkJoinSumCalculator(numbers, start + length/2, end);
+    Long leftResult = leftTask.join();
+    Long rightResult = rightTask.join();
+    return leftResult + rightResult;
+  }
+
+  private long computeSequentially() {
+    long sum = 0;
+
+    for (int i = start; i < end; i++) {
+      sum += numbers[i];
+    }
+
+    return sum;
+  }
+
+}
