04de2ad3d36de7b8831acd8117f9a2e23c99c5c3
JUNGQUI <ljk2518@gmail.com>
2021-05-31 17:43:36 +0900

    stream API chapter 7 - test case
    - Fork/Join : recursive task, test code
    
    TODO
    
    - 이후 챕터 진행

---
+++ src/test/java/com/jk/spring/java/modern/chapter7/ForkJoinSumCalculatorTest.java
@@ -0,0 +1,19 @@
+package com.jk.spring.java.modern.chapter7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.Test;
+
+class ForkJoinSumCalculatorTest {
+
+  @Test
+  void computeTest() {
+    long[] targets = LongStream.rangeClosed(1, 20_000).toArray();
+    ForkJoinSumCalculator forkJoinSumCalculator = new ForkJoinSumCalculator(targets);
+    Long result = forkJoinSumCalculator.compute();
+
+    assertEquals(Arrays.stream(targets).reduce(0L, Long::sum), result);
+  }
+}
