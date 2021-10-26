a0a59849a83a9b52ce9e010bbb49166c364ec1e4
jungqui <ljk2518@gmail.com>
2021-10-26 18:53:27 +0900

    conflict 유발

--- src/test/java/com/jk/spring/java/modern/chapter15/SimpleCellTest.java
+++ src/test/java/com/jk/spring/java/modern/chapter15/SimpleCellTest.java
@@ -10,24 +10,26 @@ class SimpleCellTest {
   void cellAddTest() {
     int x = 10, y = 20;
 
+    // conflict 유발
     SimpleCell c1 = new SimpleCell("C1");
     SimpleCell c2 = new SimpleCell("C2");
-    SimpleCell c3 = new SimpleCell("C3");
-    SimpleCell c4 = new SimpleCell("C4");
+    SimpleCell c3456 = new SimpleCell("C3");
+    SimpleCell c1234578 = new SimpleCell("C4");
 
-    c1.subscribe(c3);
-    c2.subscribe(c4);
+    c1.subscribe(c3456);
+    c2.subscribe(c1234578);
 
     c1.onNext(x);
     c2.onNext(y);
 
-    assertEquals(x + y, c3.getValue() + c4.getValue());
+    // conflict 유발
+    assertEquals(x + y, c3456.getValue() + c1234578.getValue());
   }
 
   @Test
   void cellAddTestWithArithmeticCell() {
     int x = 10, y = 20;
-
+    // conflict 유발
 
     SimpleCell c1 = new SimpleCell("C1");
     SimpleCell c2 = new SimpleCell("C2");
@@ -36,14 +38,15 @@ class SimpleCellTest {
     c1.subscribe(c3);
     c2.subscribe(c3);
 
+    // conflict 유발
     c1.onNext(x);
     c3.setLeft(c3.getValue());
     assertEquals(x, c3.getValue());
     c2.onNext(y);
+    // conflict 유발
     c3.setRight(c3.getValue());
     assertEquals(x + y, c3.getValue());
     c2.onNext(y);
-    c3.setLeft(c3.getValue());
-    assertEquals(y + y, c3.getValue());
+    // conflict 유발
   }
 }
